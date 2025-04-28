eff527014d5fa9fa51a7fac7566ab82644211dbc
ssaannzzhhiikk <omarkhanovsanzh@gmail.com>
2025-04-28 21:59:09 +0500

    fixing bugs

--- src/components/CharacterPage.tsx
+++ src/components/CharacterPage.tsx
@@ -35,11 +35,11 @@ const CharacterPage: React.FC<CharacterCardProps> = ({
   const imageUrl = (() => {
     switch (true) {
       case lowerName.includes('hermione'):
-        return "public/images/Asel.png";
+        return "/public/images/Asel.png";
       case lowerName.includes('harry potter'):
-        return "public/images/Armansu.png";
+        return "/public/images/Armansu.png";
       case lowerName.includes('ron'):
-        return "public/images/igor.jpg";
+        return "/public/images/igor.jpg";
       default:
         return image || 'https://placehold.co/200x300/png?text=' + name.charAt(0);
     }
